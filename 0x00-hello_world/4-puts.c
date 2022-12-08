0ff06a09569adb71899a4e7bf63f398c909a149b
abubekersh <abakabrak@gmail.com>
2022-12-08 06:37:14 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
 
 int main(void)
 {
-char str1[]="Programming is like building a multilingual puzzle";
+char str1[]="\"Programming is like building a multilingual puzzle";
 puts(str1);
 return(0);
 }
