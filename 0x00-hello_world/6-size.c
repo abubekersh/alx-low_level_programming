f067fb3bb57db19db3e7801623e74de05c6e2b31
abubekersh <abakabrak@gmail.com>
2022-12-08 12:42:41 -0800

    Adkami sera

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+printf("size of a char: %lu byte(s)", sizeof(char));
+printf("size of an int: %lu byte(s)", sizeof(int));
+printf("size of a long int: %lu bytes(s)", sizeof(long int));
+printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("size of a float: %lu byte(s)", sizeof(float));
+return (0);
+}
