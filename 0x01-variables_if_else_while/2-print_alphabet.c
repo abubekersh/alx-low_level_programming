f79e0aad094011cb1a657bf4ecc1e0f2242a1012
abubekersh <abakabrak@gmail.com>
2022-12-09 15:30:56 -0800

    ghh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (succcess)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	return (0);
+}
