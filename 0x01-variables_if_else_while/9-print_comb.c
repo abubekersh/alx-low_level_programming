3cd87478d418fefa451dc27edc8fb98eb71434eb
abubeker <abakabrak@gmail.com>
2023-12-15 12:56:43 -0800

    comma space a number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i++);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
