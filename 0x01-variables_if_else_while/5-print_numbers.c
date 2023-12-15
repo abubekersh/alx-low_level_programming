ab85b5991aec4889bdf8183971f8eb7a029763d7
abubeker <abakabrak@gmail.com>
2023-12-15 12:22:26 -0800

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+		printf("%d", i++);
+	printf("\n");
+	return (0);
+}
