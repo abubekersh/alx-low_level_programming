9b6436f37d84331725eff738920c46f69be3b50f
abubeker <abakabrak@gmail.com>
2023-12-15 12:28:33 -0800

    0 to 9 by putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+		putchar(i++);
+	putchar(10);
+	return (0);
+}
