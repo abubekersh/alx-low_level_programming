24037385e138e7db464eb3da5588a4f2bf694f48
abubeker <abakabrak@gmail.com>
2024-01-08 06:40:20 -0800

    strchr 4

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,17 +8,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	char *p;
-
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (*(s + i) == c)
 		{
-			p = s + i;
-			break;
+			return (s + i);
 		}
 	}
-	return (p);
+	return (0);
 }
