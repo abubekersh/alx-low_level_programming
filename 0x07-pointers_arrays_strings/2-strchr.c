662ae444871d62a50fa9ea63527fed8fa60c3fcf
abubeker <abakabrak@gmail.com>
2024-01-08 06:20:04 -0800

    worked on finding a given char addres from a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * _strchr - return a pointer to the given char in the string
+ * @s: the given string
+ * @c: the character to be found in the string
+ * Return: pointer to the c
+ */
+char *_strchr(char *s, char c)
+{
+	char *p;
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			p = s + i;
+			break;
+		}
+		else
+		{
+			p = NULL;
+		}
+	}
+	return (p);
+}
