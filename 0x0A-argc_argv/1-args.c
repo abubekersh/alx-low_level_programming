7ccd20ab40f46e1ff165778b8a7a204878fe28a9
abubeker <abakabrak@gmail.com>
2024-01-12 11:52:00 -0800

    count the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ * @argc: the number of arguments
+ * @argv: the argumnet list
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
