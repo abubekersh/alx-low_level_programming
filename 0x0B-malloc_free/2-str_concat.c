4dbe372083cdc57e720cc2da3ba8630789f2e9bf
abubeker <abakabrak@gmail.com>
2024-01-15 06:51:13 -0800

    concatneate two strings to newly allocated string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatneate the content of s2 to s1
+ * @s1: string
+ * @s2: string
+ * Return: s1 + s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new_string;
+
+	int i;
+
+	int j;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	new_string = malloc(sizeof(s1) + sizeof(s2));
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		new_string[i] = s1[i];
+	}
+	j = i;
+	for (i = 0; s2[i] != '\0'; i++, j++)
+	{
+		new_string[j] = s2[i];
+	}
+	new_string[j] = '\0';
+	return (new_string);
+}
