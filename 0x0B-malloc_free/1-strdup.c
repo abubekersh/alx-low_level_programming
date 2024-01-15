84ec1cadcb964e91ddb45f872a7a99023e902a1d
abubeker <abakabrak@gmail.com>
2024-01-15 06:22:23 -0800

    copy string 2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 	{
 		size++;
 	}
-	new_string = malloc(size * sizeof(char));
+	new_string = malloc(size * sizeof(char) + 1);
 	if (new_string == NULL)
 	{
 		return (NULL);
