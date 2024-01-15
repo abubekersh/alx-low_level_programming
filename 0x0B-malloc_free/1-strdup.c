dd4ad29a81e89e8eed0d51d6feb70460c41f6f13
abubeker <abakabrak@gmail.com>
2024-01-15 06:18:24 -0800

    copy the content of a string into a new dynamicly allocated string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicates the value of str into another
+ * @str: the given string
+ * Return: pointer to the newly allocated string
+ */
+char *_strdup(char *str)
+{
+	int size;
+
+	char *new_string;
+
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = 0;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	new_string = malloc(size * sizeof(char));
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		new_string[i] = str[i];
+	}
+	new_string[i] = '\0';
+	return (new_string);
+}
