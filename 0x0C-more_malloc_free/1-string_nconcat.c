302ed8296cbef74f0ba13794cf861f41c0baa214
abubeker <abakabrak@gmail.com>
2024-01-18 09:40:56 -0800

    concatenate s2 on s1 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	p = malloc(sizeof(char) * (_strlen(s1) + n));
+	p = malloc(sizeof(char) * (_strlen(s1) + n) + 1);
 	if (p == NULL)
 	{
 		return (NULL);
