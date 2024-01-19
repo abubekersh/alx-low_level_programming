84ecaaa53281545d9b1a67199feeaf4f4899e47d
abubeker <abakabrak@gmail.com>
2024-01-19 01:04:36 -0800

    realloc 2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,6 +14,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	unsigned int i;
 
+	unsigned int copy;
+
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -34,7 +36,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			return (NULL);
 		}
-		for (i = 0; i < old_size; i++)
+		copy = old_size;
+
+		if (new_size < old_size)
+		{
+			copy = new_size;
+		}
+		for (i = 0; i < copy; i++)
 		{
 			new_ptr[i] = ((char *) ptr)[i];
 		}
