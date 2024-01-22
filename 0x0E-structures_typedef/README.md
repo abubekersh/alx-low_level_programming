81de10cef9f5a0923fd77d86773ee917787398fc
abubeker <abakabrak@gmail.com>
2024-01-22 10:40:32 -0800

    struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+s project 0x0E-structures_typedef is about using structures annd type definitions in c and contains 6  differnet tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -  define a structture called dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: define the structure dog with the three members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
