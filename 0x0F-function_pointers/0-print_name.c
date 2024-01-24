6dc4e950ed848b5a8939e6d3e2f0b8287891b025
abubeker <abakabrak@gmail.com>
2024-01-24 09:31:21 -0800

    print name callback function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the name using the function given
+ * @name: the parameter
+ * @f: the callback function
+ * Ruturn: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this project in alx-low_level_programming is about function pointers and how to use them
