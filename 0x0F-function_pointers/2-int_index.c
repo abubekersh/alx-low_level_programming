f94b26b31cb8f12380757b962ade60baa01137f7
abubeker <abakabrak@gmail.com>
2024-01-24 11:25:27 -0800

    return te index of the first found element

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - return the index of the first elemnt
+ * @array: the array
+ * @size: size of the array
+ * @cmp: the function that compares the array elements
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL && cmp == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
