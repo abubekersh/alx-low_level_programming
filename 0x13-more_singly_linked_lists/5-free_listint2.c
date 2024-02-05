ab6285ab689071815a1d72bf941e4f5e3f313899
abubeker <abakabrak@gmail.com>
2024-02-05 09:35:14 -0800

    free the memory allocated by add and add_end then assigning head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory allocated by add and make head null
+ * @head: head of the linked list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *itr;
+
+	listint_t *temp;
+
+	itr = *head;
+	while (itr != NULL)
+	{
+		temp = itr->next;
+		free(itr);
+		itr = temp;
+	}
+	*head = NULL;
+}
