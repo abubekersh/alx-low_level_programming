0e8f9ca49ebc8acf46c1c28bdbd1188304641491
abubeker <abakabrak@gmail.com>
2024-02-06 06:08:57 -0800

    dd node at the specific position 3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		*head= new_node;
+		*head = new_node;
+		return (new_node);
 	}
 	for (i = 0; itr != NULL; i++)
 	{
