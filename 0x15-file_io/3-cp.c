024f4bd87e0ae15d4a3b140db854c4cfdecf7326
abubeker <abakabrak@gmail.com>
2024-02-12 11:59:28 -0800

    copy from one file to another

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * copy_to - copy from one file to other
+ * @file_from: src
+ * @file_to: dest
+ * Return: void
+ */
+void copy_to(const char *file_from, const char *file_to)
+{
+	int fd1;
+	int fd2;
+	char ch[1024];
+	ssize_t bwr;
+	ssize_t brd;
+
+	fd1 = open(file_from, O_RDONLY);
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd1 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	if (fd2 == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	while ((brd = read(fd1, &ch, 1024)) != 0)
+	{
+		bwr = write(fd2, &ch, brd);
+		if (bwr == -1)
+		{
+			close(fd1);
+			close(fd2);
+			dprintf(STDOUT_FILENO,"Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd2);
+                exit(100);
+	}
+	close(fd1);
+	close(fd2);
+}
+
+/**
+ * main - entry of the programm
+ * @argv: argument vector
+ * @argc: arguments size
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	
+	if (argc != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	copy_to(argv[1], argv[2]);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
